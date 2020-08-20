d16cd808be2876bd9899ef38ca890ce498b1da28
sakshi-salokhe <sakshisalokhe1@gmail.com>
2020-08-20 16:06:29 -0400

    Add files via upload

--- api/server.js
+++ api/server.js
@@ -1,8 +1,37 @@
 const http = require('http');
+const fs = require("fs");
+const url = require("url");
 const serverapp = require('./serverapp');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3600;
+const lookup = require("mime-types").lookup;
+const server = http.createServer((serverapp,res)=>
+{
+    let parsedURL = url.parse(serverapp.url, true);
+    //remove the leading and trailing slashes
+    let path = parsedURL.path.replace(/^\/+|\/+$/g, "");
 
-const server = http.createServer(serverapp);
+    if (path == "home") {
+        path = "index.html";
+      }
+
+      let file = "C:/Users/w501738/Desktop/Z-PrintingProject/api/static_files/" + path;
+
+      fs.readFile(file, function(err, content) {
+        if (err) {
+          console.log(`File Not Found ${file}`);
+          res.writeHead(404);
+          res.end();
+        } else {
+          //specify the content type in the response
+          console.log(`Returning ${path}`);
+          res.setHeader("X-Content-Type-Options", "nosniff");
+          let mime = lookup(path);
+          res.writeHead(200, { "Content-type": mime });
+          
+          res.end(content);
+        }
+      });
+});
 
 server.listen(port);
